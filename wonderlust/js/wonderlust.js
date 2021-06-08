b83605e9578b3c3e3ed5abfbcd1978106697a7c7
Rob-94 <82177403+Rob-94@users.noreply.github.com>
2021-06-08 19:46:32 +0400

    Update wonderlust.js

--- wonderlust/js/wonderlust.js
+++ wonderlust/js/wonderlust.js
@@ -1,6 +1,8 @@
 let input = document.getElementById("inp")
 const getTemp = async ()=> {let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=d7085e16a4f596f2a4c2257f0348ae50`);
 const data = await  response.json()
+
+
 let temp = Math.floor(data.main.temp - 273);
 let wind = data.wind.speed;
 let today = new Date();
@@ -10,6 +12,7 @@ let day = today.getDate()
 let days =['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 let weekday = days[today.getDay()];
 let time = `${today.getHours()}:${today.getMinutes()}`
+
 
 let timemoji =String.fromCodePoint(0x0001F555)
 let cloudEmoji =String.fromCodePoint(0x00026C5)
@@ -26,9 +29,6 @@ let skyEmoji =String.fromCodePoint(0x00001F30E)
 let paragraph1 =document.createElement("p");
 let inner1 = document.createTextNode(`${day}/${month}/${year} ${calendarEmoji} `);
 paragraph1.appendChild(inner1)
-
-
-
 
 let paragraph2 =document.createElement("p");
 let inner2 = document.createTextNode(`${time} ${timemoji}  `);
@@ -58,14 +58,12 @@ box.appendChild(paragraph3)
 box.appendChild(paragraph4)
 box.appendChild(paragraph5)
 box.appendChild(paragraph6)
-
 box.setAttribute("class","weather")
-
-let inpInner =document.getElementById("inp").value
+                            
 let desc = document.getElementById("description")
 
 let p1 =document.createElement("p");
-let pI =document.createTextNode(inpInner);
+let pI =document.createTextNode(input.value);
 p1.appendChild(pI);
 p1.setAttribute("class","blue");
 desc.appendChild(p1);
